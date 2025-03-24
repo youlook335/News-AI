8795cdb1d4a67bca06c8ddcd8a534b435d0c4b95
unknown <youloos477@gmail.com>
2025-03-24 18:28:33 +0500

    Find A Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
@@ -9,14 +10,14 @@ import Football from "./pages/Football";
 import CricketNews from "./pages/CricketNews";
 import BasketballNews from "./pages/BasketballNews"
 import HockeyNews from "./pages/HockeyNews"
-import TennisNews from "./pages/TennisNews"  
-import Boxing from "./pages/Boxing" 
-import Cycling from "./pages/Cycling" 
-import StockMarketNews from "./pages/StockMarketNews" 
-import CryptoNews from "./pages/CryptoNews" 
-import BankingNews from "./pages/BankingNews" 
-import RealEstateNews from "./pages/RealEstateNews"  
-import Forex from "./pages/Forex"  
+import TennisNews from "./pages/TennisNews"
+import Boxing from "./pages/Boxing"
+import Cycling from "./pages/Cycling"
+import StockMarketNews from "./pages/StockMarketNews"
+import CryptoNews from "./pages/CryptoNews"
+import BankingNews from "./pages/BankingNews"
+import RealEstateNews from "./pages/RealEstateNews"
+import Forex from "./pages/Forex"
 import NewsDetail from "./pages/NewsDetail";
 import Footer from "./components/Footer";
 
@@ -40,18 +41,18 @@ function App() {
         <Route path="/tennis" element={<TennisNews />} />
         {/* Technology */}
         <Route path="/technology" element={<Technology />} />
-        
+
         {/* Finannce */}
         <Route path="/stock-market" element={<StockMarketNews />} />
         <Route path="/crypto" element={<CryptoNews />} />
-        <Route path="/real-estate" element={<RealEstateNews />} />;
+        <Route path="/real-estate" element={<RealEstateNews />} />
         <Route path="/banking" element={<BankingNews />} />
         <Route path="/forex" element={<Forex />} />
 
         {/* World News */}
         <Route path="/world-news" element={<WorldNews />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
